46ee49e6977d34d7e0a03e0118c158ae1c965180
Ashley <ashleyaggie@aol.com>
2021-05-05 17:00:56 -0500

    Added function for get one product.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,10 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productsData = await Product.findAll({
       include: [{model: Category}, { model: Tag, through: ProductTag, as: 'product_tags' }]
     });
-    res.status(200).json(productData);
+    res.status(200).json(productsData);
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
@@ -19,9 +19,18 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
