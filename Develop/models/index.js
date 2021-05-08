3cd47d589ede457b73479c3be5c51fb8d8520c1e
Ashley <ashleyaggie@aol.com>
2021-05-07 23:43:50 -0500

    Added FK to line 27 and 37 instead of as.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -24,7 +24,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: 'product_tags'
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -34,7 +34,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'tagged_products'
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
